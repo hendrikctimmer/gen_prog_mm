edf280a05cb0a925552018964533cffae60240d4
hendrikctimmer <hendrikctimmer@gmail.com>
2023-03-09 12:17:07 +0100

    added algorithm class

---
+++ Algorithm.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Algorithm:
+
+	def __init__(self):
+	
+		self.mult_algo = {}
+		
--- Matmul.py
+++ Matmul.py
@@ -1,15 +1,22 @@
 import numpy as np
+import algorithm
 
 class Matmul:
 
+	"""Object is initialized with number of triples as parameter, where the triple is
+	a triple of matrices (a,b,c), such that a*b=c. 
+	
+	mult_algo is a dictionary where the 
+	
+	"""
+
 	def __init__(self, num_triples):
-		
+
 		self.matrices = []
 		self.num_triples = num_triples
 		self.mult_algo = {}
 		self.mult_algo_nums = {}
 		
-
 	def init_mats(self, num_triples):
 		
 		for x in range(num_triples):
@@ -33,13 +40,10 @@ class Matmul:
 				res_int_list = []							
 				for z in range(res.shape[1]):	
 					res_int_list.append(m1[x][z]*m2[z][y])
-					self.mult_algo["h" + str(mult_num)] = "a" + str(x+1) + str(z+1) + " * " + "b" + str(z+1) + str(y+1)
+					self.mult_algo["h" + str(mult_num+1)] = "a" + str(x+1) + str(z+1) + " * " + "b" + str(z+1) + str(y+1)
 					self.mult_algo_nums["h" + str(mult_num)] = str(m1[x][z]) + " * " + str(m2[z][y])
 					mult_num += 1
 				res[x][y] = sum(res_int_list)
-		#for x in range(len(self.mult_algo_nums.keys())):
-		#	print(self.mult_algo["h"+str(x)])
-		#	print(self.mult_algo_nums["h"+str(x)])
 		return res
 			
 	def assess_mult(self, result_mat, answer_mat):
@@ -48,6 +52,12 @@ class Matmul:
 			for y in range(result_mat.shape[1]):
 				if result_mat[x][y] != answer_mat[x][y]:
 					print("Error in position ", x,",", y, "is: ",result_mat[x][y], " should be: ", answer_mat[x][y])
+					
+	def init_pop(self, pop_size):
+		pass
+		
+		
+	def rand_algo(self, num_):
 		
 	def main(self):
 
@@ -58,6 +68,11 @@ class Matmul:
 		print(self.matrices[0][2])
 		
 		print(self.mat_mult(self.matrices[0][0],self.matrices[0][1]))
+		
+		for x, y, z  in zip(self.mult_algo_nums.values(), self.mult_algo.keys(), self.mult_algo.values()):
+			print(y)
+			print(z)
+			print(x + " = " + str(eval(x)) + "\n")
 		
 		
 		
